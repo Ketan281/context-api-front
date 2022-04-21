dd382f3e8d95d829e64efcb040e118fff4ad9af3
Ketan Mohite <91816844+Ketan281@users.noreply.github.com>
2022-04-21 16:10:13 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,10 +7,10 @@ import {
 
 export const Navbar = () => {
     return (<div>
-        <Link to='/'>Home</Link>
-        <Link to='/About'>About</Link>
-        <Link to='/contact'>Contact</Link>
-        <Link to='/users'>Users</Link>
-        <Link to='/login'>Login</Link>
+        <Link to='/'>Home</Link>&nbsp&nbsp
+        <Link to='/About'>About</Link>&nbsp&nbsp
+        <Link to='/contact'>Contact</Link>&nbsp&nbsp
+        <Link to='/users'>Users</Link>&nbsp&nbsp
+        <Link to='/login'>Login</Link>&nbsp&nbsp
     </div>);
 }
